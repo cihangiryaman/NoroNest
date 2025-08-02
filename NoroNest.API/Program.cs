95ea989a67c55c1152a5cc35a5dbbcad8adf9a9c
cihangiryaman <cihangiryaman3200@gmail.com>
2025-08-02 16:19:14 +0300

    Logging added

--- NoroNest.API/Program.cs
+++ NoroNest.API/Program.cs
@@ -1,3 +1,26 @@
+using NoroNest.Application;
+using Serilog;
+using Serilog.Events;
+
+Log.Logger = new LoggerConfiguration()
+	.MinimumLevel.Debug()
+	.WriteTo.Logger(lc => lc
+		.Filter.ByIncludingOnly(e => e.Level == LogEventLevel.Information)
+		.WriteTo.File(
+			path: "Logs/info/info-.txt",
+			rollingInterval: RollingInterval.Day,
+			outputTemplate: "[{Timestamp:yyyy-MM-dd HH:mm:ss} {Level:u3}] {Message:lj}{NewLine}{Exception}",
+			retainedFileCountLimit: 7))
+	.WriteTo.Logger(lc => lc
+		.Filter.ByIncludingOnly(e => e.Level == LogEventLevel.Error || e.Level == LogEventLevel.Fatal)
+		.WriteTo.File(
+			path: "Logs/errors/error-.txt",
+			rollingInterval: RollingInterval.Day,
+			outputTemplate: "[{Timestamp:yyyy-MM-dd HH:mm:ss} {Level:u3}] {Message:lj}{NewLine}{Exception}",
+			retainedFileCountLimit: 30))
+	.WriteTo.Console()
+	.CreateLogger();
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +29,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.RegisterNoroNest(builder.Configuration);
 
 var app = builder.Build();
 
@@ -17,7 +42,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
